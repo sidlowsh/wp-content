a3322975631ba27e704ab75c43acd0aa6f78396a
Sarah <sarah.sidlow@georgetown.edu>
2019-04-03 14:52:11 -0400

    Clean up notes

--- plugins/Events/events.php
+++ plugins/Events/events.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * @package Job_Opportunities
+ * @package Reading_List
  * @version 0.1
  */
 /*
-Plugin Name: Job Opps
+Plugin Name: Reading List
 Plugin URI: http://andrewspittle.net/projects/reading-list
-Description: Track and display job opportunities.
-Author: Andrew Spittle; Adapted by Sarah Sidlow
+Description: Track the books you read right from the WordPress Dashboard.
+Author: Andrew Spittle
 Version: 0.1
 Author URI: http://andrewspittle.net/
 */
@@ -22,15 +22,15 @@ add_action( 'init', 'rl_create_post_type' );
 
 function rl_create_post_type() {
 	$labels = array(
-		'name' 							=> __( 'Job Opps', 'readinglist' ),
-		'singular_name' 				=> __( 'Job', 'readinglist' ),
-		'search_items'					=> __( 'Search Jobs', 'readinglist' ),
-		'all_items'						=> __( 'All Jobs', 'readinglist' ),
-		'edit_item'						=> __( 'Edit Job', 'readinglist' ),
-		'update_item' 					=> __( 'Update Job', 'readinglist' ),
-		'add_new_item' 					=> __( 'Add New Job', 'readinglist' ),
-		'new_item_name' 				=> __( 'New Job', 'readinglist' ),
-		'menu_name' 					=> __( 'Jobs', 'readinglist' ),
+		'name' 							=> __( 'Books', 'readinglist' ),
+		'singular_name' 				=> __( 'Book', 'readinglist' ),
+		'search_items'					=> __( 'Search Books', 'readinglist' ),
+		'all_items'						=> __( 'All Books', 'readinglist' ),
+		'edit_item'						=> __( 'Edit Book', 'readinglist' ),
+		'update_item' 					=> __( 'Update Book', 'readinglist' ),
+		'add_new_item' 					=> __( 'Add New Book', 'readinglist' ),
+		'new_item_name' 				=> __( 'New Book', 'readinglist' ),
+		'menu_name' 					=> __( 'Books', 'readinglist' ),
 	);
 	
 	$args = array (
@@ -45,7 +45,7 @@ function rl_create_post_type() {
 }
 
 /**
- * Create our custom taxonomies. One hierarchical one for genres and a flat one for employers.
+ * Create our custom taxonomies. One hierarchical one for genres and a flat one for authors.
  *
  * @since Reading List 0.1
  */
@@ -56,17 +56,17 @@ add_action( 'init', 'rl_create_book_taxonomies', 0 );
 function rl_create_book_taxonomies() {
 	// Add new taxonomy, keep it non-hierarchical (like tags)
 	$labels = array(
-		'name' 							=> __( 'Skills', 'readinglist' ),
-		'singular_name' 				=> __( 'Skill', 'readinglist' ),
-		'search_items' 					=> __( 'Search skills', 'readinglist' ),
-		'all_items' 					=> __( 'All skills', 'readinglist' ),
-		'edit_item' 					=> __( 'Edit skills', 'readinglist' ), 
-		'update_item' 					=> __( 'Update skills', 'readinglist' ),
-		'add_new_item' 					=> __( 'Add New skill', 'readinglist' ),
-		'new_item_name' 				=> __( 'New skill', 'readinglist' ),
-		'separate_items_with_commas' 	=> __( 'Separate skills with commas', 'readinglist' ),
-		'choose_from_most_used' 		=> __( 'Choose from the most used skills', 'readinglist' ),
-		'menu_name' 					=> __( 'Skills', 'readinglist' ),
+		'name' 							=> __( 'Authors', 'readinglist' ),
+		'singular_name' 				=> __( 'Author', 'readinglist' ),
+		'search_items' 					=> __( 'Search Authors', 'readinglist' ),
+		'all_items' 					=> __( 'All Authors', 'readinglist' ),
+		'edit_item' 					=> __( 'Edit Author', 'readinglist' ), 
+		'update_item' 					=> __( 'Update Author', 'readinglist' ),
+		'add_new_item' 					=> __( 'Add New Author', 'readinglist' ),
+		'new_item_name' 				=> __( 'New Author Name', 'readinglist' ),
+		'separate_items_with_commas' 	=> __( 'Separate authors with commas', 'readinglist' ),
+		'choose_from_most_used' 		=> __( 'Choose from the most used authors', 'readinglist' ),
+		'menu_name' 					=> __( 'Authors', 'readinglist' ),
 	); 	
 		
 	register_taxonomy( 'book-author', array( 'rl_book' ), array(
@@ -75,12 +75,12 @@ function rl_create_book_taxonomies() {
 		'show_ui' 			=> true,
 		'show_admin_column' => true,
 		'query_var' 		=> true,
-		'rewrite' 			=> array( 'slug' => 'employer' ),
+		'rewrite' 			=> array( 'slug' => 'book-author' ),
 	));
 }
 
 /**
- * Add custom meta box for tracking the employer.
+ * Add custom meta box for tracking the page numbers of the book.
  *
  * Props to Justin Tadlock: http://wp.smashingmagazine.com/2011/10/04/create-custom-post-meta-boxes-wordpress/
  *
@@ -99,16 +99,16 @@ function rl_post_meta_boxes_setup() {
 	add_action( 'add_meta_boxes', 'rl_add_post_meta_boxes' );
 	
 	/* Save post meta on the 'save_post' hook. */
-	add_action( 'save_post', 'rl_employer_save_meta', 10, 2 );
+	add_action( 'save_post', 'rl_pages_save_meta', 10, 2 );
 }
 
 /* Create one or more meta boxes to be displayed on the post editor screen. */
 function rl_add_post_meta_boxes() {
 
 	add_meta_box(
-		'rl-employer',								// Unique ID
-		esc_html__( 'Employer', 'example' ),		// Title
-		'rl_employer_meta_box',					// Callback function
+		'rl-pages',								// Unique ID
+		esc_html__( 'Pages', 'example' ),		// Title
+		'rl_pages_meta_box',					// Callback function
 		'rl_book',								// Add metabox to our custom post type
 		'side',									// Context
 		'default'								// Priority
@@ -116,19 +116,19 @@ function rl_add_post_meta_boxes() {
 }
 
 /* Display the post meta box. */
-function rl_employer_meta_box( $object, $box ) { ?>
+function rl_pages_meta_box( $object, $box ) { ?>
 
-	<?php wp_nonce_field( basename( __FILE__ ), 'rl_employer_nonce' ); ?>
+	<?php wp_nonce_field( basename( __FILE__ ), 'rl_pages_nonce' ); ?>
 
-	<p class="howto"><label for="rl-employer"><?php _e( "Add the employer name here.", 'example' ); ?></label></p>
-	<p><input class="widefat" type="text" name="rl-pages" id="rl-pages" value="<?php echo esc_attr( get_post_meta( $object->ID, 'rl_employer', true ) ); ?>" size="30" /></p>
+	<p class="howto"><label for="rl-pages"><?php _e( "Add the page count of the book.", 'example' ); ?></label></p>
+	<p><input class="widefat" type="text" name="rl-pages" id="rl-pages" value="<?php echo esc_attr( get_post_meta( $object->ID, 'rl_pages', true ) ); ?>" size="30" /></p>
 <?php }
 
 /* Save the meta box's data. */
-function rl_employer_save_meta( $post_id, $post ) {
+function rl_pages_save_meta( $post_id, $post ) {
 
 	/* Verify the nonce before proceeding. */
-	if ( !isset( $_POST['rl_employer_nonce'] ) || !wp_verify_nonce( $_POST['rl_employer_nonce'], basename( __FILE__ ) ) )
+	if ( !isset( $_POST['rl_pages_nonce'] ) || !wp_verify_nonce( $_POST['rl_pages_nonce'], basename( __FILE__ ) ) )
 		return $post_id;
 
 	/* Get the post type object. */
@@ -139,10 +139,10 @@ function rl_employer_save_meta( $post_id, $post ) {
 		return $post_id;
 
 	/* Get the posted data and sanitize it for use as an HTML class. */
-	$new_meta_value = ( isset( $_POST['rl-employer'] ) ? sanitize_html_class( $_POST['rl-employer'] ) : '' );
+	$new_meta_value = ( isset( $_POST['rl-pages'] ) ? sanitize_html_class( $_POST['rl-pages'] ) : '' );
 
 	/* Get the meta key. */
-	$meta_key = 'rl_employer';
+	$meta_key = 'rl_pages';
 
 	/* Get the meta value of the custom field key. */
 	$meta_value = get_post_meta( $post_id, $meta_key, true );
@@ -159,7 +159,5 @@ function rl_employer_save_meta( $post_id, $post ) {
 	elseif ( '' == $new_meta_value && $meta_value )
 		delete_post_meta( $post_id, $meta_key, $meta_value );
 } 
-
-
 
 ?>
