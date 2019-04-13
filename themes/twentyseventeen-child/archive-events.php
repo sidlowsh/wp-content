5e52869591d2ab9632d30bbcdcf27b091a410831
Sarah <sarah.sidlow@georgetown.edu>
2019-04-12 21:28:11 -0400

    Commit custom 404 page

--- themes/twentyseventeen-child/archive-events.php
+++ themes/twentyseventeen-child/archive-events.php
@@ -15,9 +15,9 @@ get_header(); ?>
 <div class="wrap">
 
 	<?php if ( have_posts() ) : ?>
-		<header class="page-header events-page-header">
+		<header class="page-header">
 			<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
+				the_archive_title( '<h1 class="page-title event-page-title">', '</h1>' );
 				the_archive_description( '<div class="taxonomy-description">', '</div>' );
 			?>
 		</header><!-- .page-header -->
