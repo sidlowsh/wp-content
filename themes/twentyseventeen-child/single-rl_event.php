1118f13f4bb668a9f51e78ed357aadae3658c546
Sarah <sarah.sidlow@georgetown.edu>
2019-04-19 13:39:03 -0400

    Commit custom event template to display metabox data

---
+++ themes/twentyseventeen-child/single-rl_event.php
@@ -0,0 +1,55 @@
+<?php
+/*Template Name: Event Post
+ * Template Post Type: post, page, product, event
+ */
+  
+ get_header();  ?>
+
+<div class="wrap">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			
+			<span class="meta">
+				<p>When: <?php echo get_post_meta( $post->ID , 'ev_date', true ); ; ?><br>
+				Where: <?php echo get_post_meta( $post->ID , 'ev_location', true ); ; ?> <br>
+				RSVP: <?php echo get_post_meta( $post->ID , 'ev_link', true ); ; ?></p>
+				
+			</span>
+
+
+			<?php
+
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();
+
+				get_template_part( 'template-parts/post/content', get_post_format() );
+
+				//display data from metabox on frontend	
+				echo get_post_meta( $post->ID , 'ev_date', true );
+				echo get_post_meta( $post->ID , 'ev_location', true );
+				echo get_post_meta( $post->ID , 'ev_link', true );
+
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+				the_post_navigation(
+					array(
+						'prev_text' => '<span class="screen-reader-text">' . __( 'Previous Event', 'twentyseventeen' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Previous', 'twentyseventeen' ) . '</span> <span class="nav-title"><span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '</span>%title</span>',
+						'next_text' => '<span class="screen-reader-text">' . __( 'Next Event', 'twentyseventeen' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Next', 'twentyseventeen' ) . '</span> <span class="nav-title">%title<span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ) . '</span></span>',
+					)
+				);
+
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+	<?php get_sidebar(); ?>
+</div><!-- .wrap -->
+
+<?php
+get_footer();
